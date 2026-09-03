062cfe47c52f0d19a4711137243f83422413285d
Torque <noor.mudassir@outlook.com>
2026-09-02 21:58:37 -0700

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        cache = [0 for _ in nums]
+        houses = len(nums) - 1
+    
+        for h in range(houses, -1, -1):
+            if h > houses - 2:
+                cache[h] = nums[h]
+            else:
+                if h + 3 <= houses:
+                    cache[h] = nums[h] + max(cache[h + 2], cache[h + 3])
+                else:
+                    cache[h] = nums[h] + cache[h + 2]
+
+        if len(cache) > 1:
+            return max(cache[0], cache[1])
+        else:
+            return cache[0]
